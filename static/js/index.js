62209a1a32ca454db730f993f52d76308fc70bfb
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-02-01 15:03:09 -0300

    feat: Add commands history

--- static/js/index.js
+++ static/js/index.js
@@ -4,88 +4,23 @@ const inputContainer = document.querySelector('.inputContainer')
 const inputContainerInput = document.querySelector('.inputContainer input')
 const inputContainerSpan = document.querySelector('.inputContainer span')
 
+const HISTORY = []
+const MAX_HISTORY = 200
+let HISTORY_INDEX = -1
 
-const DOWNLOADS = {
-	name: 'Downloads',
-	parent: () => HOME,
-	isDir: true,
-	childs: [
-		{
-			isDir: false,
-			name: 'music.mp3',
-			isBinary: true,
-			content: null
-		},
-		{
-			isDir: false,
-			name: 'movie.mp4',
-			isBinary: true,
-			content: null
-		},
-		{
-			isDir: false,
-			name: 'text.txt',
-			isBinary: false,
-			content: 'Lorem ipsum dolor sit amet.'
-		},
-	]
-}
-
-const DOCUMENTS = {
-	name: 'Documents',
-	parent: () => HOME,
-	isDir: true,
-	childs: [
-		{
-			name: 'empty-folder',
-			parent: () => DOCUMENTS,
-			isDir: true,
-			childs: []
-		},
-		{
-			isDir: false,
-			name: 'work.txt',
-			isBinary: false,
-			content: 'This is a important work.'
-		},
-	]
-}
-
-const HOME = {
-	name: 'Home',
-	parent: () => null,
-	isDir: true,
-	childs: [
-		DOWNLOADS,
-		DOCUMENTS,
-		{
-			isDir: false,
-			name: 'welcome.txt',
-			isBinary: false,
-			content: 'Welcome to my and your CMD. Enjoy!'
-		}
-	]
-}
-
-const actualDirectory = HOME
-
-function makeDirPath() {
-	const paths = []
-	let dirRef = actualDirectory
-
-	paths.push(dirRef.name)
-	while (dirRef.parent() !== null) {
-		dirRef = dirRef.parent()
-		paths.push(dirRef.name)
-		console.log(paths)
-	}
-
-	return `C:\\Users\\${paths.reverse().join('\\')}>`
-}
+// Functions
 
 function parseCommand(value) {
 	const args = value.split(' ').map(item => item.trim())
 	const arg = args.shift()
+
+	HISTORY.push(arg)
+
+	if (HISTORY.length > MAX_HISTORY) {
+		while (HISTORY.length > MAX_HISTORY) HISTORY.shift()
+	}
+
+	HISTORY_INDEX = HISTORY.length - 1
 
 	const commands = {
 		dir: command_dir,
@@ -105,12 +40,35 @@ function parseCommand(value) {
 }
 
 
+// Events and Methods
+
 inputContainerInput.focus()
 inputContainerInput.addEventListener('keydown', e => {
-	if (e.key !== 'Enter') return
+	const key = e.key
+	const value = e.currentTarget.value
 
-	parseCommand(e.currentTarget.value)
-	e.currentTarget.value = ''
+	if (key === 'Enter') {
+		parseCommand(value)
+		e.currentTarget.value = ''
+
+	} else if ((key === 'ArrowUp' || key === 'ArrowDown') && HISTORY.length > 0) {
+		console.log(HISTORY, HISTORY_INDEX)
+
+		if (key === 'ArrowUp' && HISTORY_INDEX < 0) return
+		if (key === 'ArrowDown' && HISTORY_INDEX >= HISTORY.length) return
+
+		e.currentTarget.value = HISTORY[HISTORY_INDEX]
+		
+		if (key === 'ArrowUp') {
+			HISTORY_INDEX--
+			if (HISTORY_INDEX < 0) HISTORY_INDEX = 0
+		}
+		if (key === 'ArrowDown') {
+			HISTORY_INDEX++
+			if (HISTORY_INDEX > HISTORY.length - 1) HISTORY_INDEX = HISTORY.length - 1
+		}
+
+	}
 })
 
 inputContainerSpan.innerText = makeDirPath()
